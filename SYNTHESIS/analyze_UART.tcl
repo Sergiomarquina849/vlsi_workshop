bbfcc36c51e4e7c516143b614d5076ea89f6c3fc
Sergiomarquina849 <sergio00001sergio@gmail.com>
2026-02-12 05:20:53 -0500

    Add files via upload

---
+++ SYNTHESIS/analyze_UART.tcl
@@ -0,0 +1,11 @@
+analyze -library $design_library -format sverilog \
+{/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/BaudGenR.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/BaudGenT.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/DeFrame.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/Duplex.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/ErrorCheck.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/Parity.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/PISO.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/RxUnit.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/SIPO.v
+/home/vlsi26/1ds23ec088-413-c2s/SYNTHESIS/Design_UART/RTL/TxUnit.v}
